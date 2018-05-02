2fc001e4226b1f58c5afdf741bb38f1f63a29001
Herve <dockerlite@gmail.com>
2018-05-02 13:07:05 +0200

    update

--- filter_plugins/my_filters.py
+++ filter_plugins/my_filters.py
@@ -2,14 +2,11 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'a_filter': self.a_filter,
-            'another_filter': self.b_filter
+            'a_filter': self.a_filter
         }
  
     def a_filter(self, a_variable):
         a_new_variable = a_variable + ' CRAZY NEW FILTER'
         return a_new_variable
 
-    def b_filter(self, a_variable, another_variable, yet_another_variable):
-        a_new_variable = a_variable + ' - ' + another_variable + ' - ' + yet_another_variable
-        return a_new_variable
+
